36f098a5fa58f3daa2ad286afa1c07e25c03b62f
amabraha <107161721+amabraha@users.noreply.github.com>
2023-10-02 19:47:52 -0400

    Update thing.sv

--- src/thing.sv
+++ src/thing.sv
@@ -2,11 +2,27 @@
 
 
 module tt_um_calculator_chip
-  (input logic clock, Enter, Reset,
-   input logic [7:0] NumIn,
-   input logic [1:0] OpIn,
-   output logic [7:0] NumOut
+  (
+   input  logic [7:0] ui_in,    // Dedicated inputs - connected to the input switches
+    output logic [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
+    input  logic [7:0] uio_in,   // IOs: Bidirectional Input path
+    output logic [7:0] uio_out,  // IOs: Bidirectional Output path
+    output logic [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
+    input  logic       ena,      // will go high when the design is enabled
+    input  logic       clk,      // clock
+    input  logic       rst_n     // reset_n - low to reset
   );
+  logic clock, Reset;
+  logic [1:0] OpIn;
+  logic [7:0] NumIn, NumOut;
+  logic Enter;
+  assign clock = clk;
+  assign Reset = rst_n;
+  assign OpIn=uio_in[1:0];
+  assign uo_out = NumOut;
+  assign NumIn = ui_in;
+  assign Enter = uio_in[2];
+  assign uio_oe = 8'b0;
   
   enum logic {ME, NOTME}button;
   
